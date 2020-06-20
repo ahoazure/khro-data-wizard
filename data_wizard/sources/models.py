3d223f53471caae7a3fc398d151953a694b23043
Stephen <smburu@uonbi.ac.ke>
2020-06-20 23:44:53 +0300

    Added location field in thr data source

--- data_wizard/sources/models.py
+++ data_wizard/sources/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from regions.models import StgLocation
 
-
-class FileSource(models.Model):
-    name = models.CharField(max_length=255, null=True, blank=True)
-    file = models.FileField(upload_to='datawizard/')
+class FileSource(models.Model): #added location lookup for location here but may need to chnage
+    location = models.ForeignKey(StgLocation, models.PROTECT,
+		verbose_name = 'Location',default=1,)
+    name = models.CharField(max_length=255, null=True, 
+		blank=True,verbose_name = 'File Name',)
+    file = models.FileField(upload_to='datawizard/',
+		verbose_name = 'Attach File',)
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -11,7 +15,10 @@ class FileSource(models.Model):
 
 
 class URLSource(models.Model):
-    name = models.CharField(max_length=255, null=True, blank=True)
+    location = models.ForeignKey(StgLocation, models.PROTECT,
+		verbose_name = 'Location',default=1,)
+    name = models.CharField(max_length=255, null=True, 
+		blank=True,verbose_name = 'Link Name',)
     url = models.URLField()
     date = models.DateTimeField(auto_now_add=True)
 
