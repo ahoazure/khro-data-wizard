18f630e8d7d20b97ed3efee2d95ee9bf53f861a7
Stepcloud <ahoazure@gmail.com>
2021-02-03 11:14:45 +0300

    Added the location field on data wizard source models

--- data_wizard/sources/models.py
+++ data_wizard/sources/models.py
@@ -1,20 +1,26 @@
 from django.db import models
 from khro_app.authentication.models import CustomUser
+from khro_app.regions.models import StgLocation
 
-class FileSource(models.Model): #added location lookup for location here but may need to chnage
+class FileSource(models.Model): #added location lookup for CD/CI
+    location = models.ForeignKey(StgLocation, models.PROTECT,
+		verbose_name = 'Location',)
     user = models.ForeignKey(CustomUser, models.PROTECT,blank=False,
 		verbose_name = 'User Name (Email)',default=4)
     name = models.CharField(max_length=255, null=True,
 		blank=True,verbose_name = 'File Name',)
     file = models.FileField(upload_to='datawizard/',
 		verbose_name = 'Source File',)
-    date = models.DateTimeField(auto_now_add=True,verbose_name='Import Date',)
+    date = models.DateTimeField(auto_now_add=True,
+		verbose_name='Import Date',)
 
     def __str__(self):
         return self.name or self.file.name
 
 
 class URLSource(models.Model):
+    location = models.ForeignKey(StgLocation, models.PROTECT,
+		verbose_name = 'Location',)
     user = models.ForeignKey(CustomUser, models.PROTECT,blank=False,
 		verbose_name = 'User Name (Email)',default=4)
     name = models.CharField(max_length=255, null=True,blank=True,
